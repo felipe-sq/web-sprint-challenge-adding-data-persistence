c843b8cf076f5f08bac8a17c2917af65857b6742
Felipe Slaughter-Quintero <fslauq@gmail.com>
2021-08-28 13:12:00 -0400

    updated table values for project and task completed to boolean instead of integers

--- data/migrations/20210828153458_first.js
+++ data/migrations/20210828153458_first.js
@@ -5,7 +5,7 @@ exports.up = async function(knex) {
     tbl.increments('project_id');
     tbl.string('project_name', 128).notNullable().unique();
     tbl.string('project_description', 255);
-    tbl.integer('project_completed').defaultTo(0);
+    tbl.boolean('project_completed').defaultTo(false);
   })
   .createTable('resources', tbl => {
     tbl.increments('resource_id');
@@ -16,7 +16,7 @@ exports.up = async function(knex) {
     tbl.increments('task_id');
     tbl.string('task_description', 255).notNullable();
     tbl.string('task_notes', 355);
-    tbl.integer('task_completed').defaultTo(0);
+    tbl.boolean('task_completed').defaultTo(false);
     tbl.integer('project_id')
       .unsigned()
       .notNullable()
